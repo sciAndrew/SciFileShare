48eed766d9f857a44afec3569d90f0353b888755
sciAndrew <44843860+sciAndrew@users.noreply.github.com>
2020-01-24 11:24:33 +0100

    0.3

--- SciFileShare.bat
+++ SciFileShare.bat
@@ -1,35 +1,37 @@
+:restart
 @echo off
 set cooldown=0
-if not exist H:\FileShare\ ( mkdir H:\FileShare )
+if not exist H:\SciFileShare\ ( mkdir H:\SciFileShare )
+if not exist P:\SciFileShare\ ( mkdir P:\SciFileShare )
 
-echo aby pobraæ plik od kogoœ stwórz w folderze FileShare plik w ten sposób [u¿ytkownik udostêpniaj¹cy plik]RQ.txt > "P:\FileShare\1 FileShare instrukca obs³ugi.txt"
-echo np.andrewRQ.txt . w œrodku teko pliku piszesz nazwê pliku który chcesz pobraæ, ten plik powinien siê pojawiæ na publicu  >> "P:\FileShare\1 FileShare instrukca obs³ugi.txt"
-echo aby wystawiaæ pliki do pobrania i po wiencej instrukcji zobacz na githuba >> "P:\FileShare\1 FileShare instrukca obs³ugi.txt"
-echo https://github.com/sciAndrew/SciFileShare >> "P:\FileShare\1 FileShare instrukca obs³ugi.txt"
+echo aby pobraæ plik od kogoœ stwórz w folderze SciFileShare plik w ten sposób [u¿ytkownik udostêpniaj¹cy plik]RQ.txt > "P:\SciFileShare\1 SciSciFileShare jak uzywac.txt"
+echo np.andrewRQ.txt . w œrodku teko pliku piszesz nazwê pliku który chcesz pobraæ, ten plik powinien siê pojawiæ na publicu  >> "P:\SciFileShare\1 SciSciFileShare jak uzywac.txt"
+echo aby wystawiaæ pliki do pobrania i po wiencej instrukcji zobacz na githuba >> "P:\SciFileShare\1 SciSciFileShare jak uzywac.txt"
+echo https://github.com/sciAndrew/SciSciFileShare >> "P:\SciFileShare\1 SciSciFileShare jak uzywac.txt"
 
 :start
-if %TIME:~3,2%==00 ( del /f /q /s P:\FileShare\ && timeout 60 )
+if %TIME:~3,2%==00 ( del /f /q /s P:\SciFileShare\ && timeout 60 && goto restart )
 echo %cooldown%
-timeout 10
+timeout 20
 if %cooldown% gtr 0 ( set /A cooldown=%cooldown%-1 )
-if not exist P:\FileShare\ ( mkdir P:\FileShare )
-if not exist P:\FileShare\%USERNAME%_list.txt ( dir /B H:\FileShare\ > P:\FileShare\%USERNAME%_FileList.txt )
-if exist P:\FileShare\%USERNAME%RQ.txt ( goto process ) else ( goto start )
+if not exist P:\SciFileShare\ ( mkdir P:\SciFileShare )
+if not exist P:\SciFileShare\%USERNAME%_list.txt ( dir /B H:\SciFileShare\ > P:\SciFileShare\%USERNAME%_FileList.txt )
+if exist P:\SciFileShare\%USERNAME%RQ.txt ( goto process ) else ( goto start )
 
 :process
 
-set /A cooldown=%cooldown%+20
+set /A cooldown=%cooldown%+10
 
-set /p requested=<P:\FileShare\%USERNAME%RQ.txt
-del /q P:\FileShare\%USERNAME%RQ.txt
+set /p requested=<P:\SciFileShare\%USERNAME%RQ.txt
+del /q P:\SciFileShare\%USERNAME%RQ.txt
 
-if exist H:\FileShare\%requested% ( copy "H:\FileShare\%requested%" P:\ )
+if exist H:\SciFileShare\%requested% ( copy "H:\SciFileShare\%requested%" P:\ )
 
-::echo %USERNAME% > "P:\FileShare\%USERNAME% on cooldown"
+::echo %USERNAME% > "P:\SciFileShare\%USERNAME% on cooldown"
 
 echo cooldown %cooldown%
 timeout %cooldown%
 
-::del /q "P:\FileShare\%USERNAME% on cooldown"
+::del /q "P:\SciFileShare\%USERNAME% on cooldown"
 
 goto start
