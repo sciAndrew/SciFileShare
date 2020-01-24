d0fcdf1426e41359e7e3a953ff63950b10376671
sciAndrew <44843860+sciAndrew@users.noreply.github.com>
2020-01-24 11:28:43 +0100

    0.4

--- SciFileShare.bat
+++ SciFileShare.bat
@@ -4,10 +4,10 @@ set cooldown=0
 if not exist H:\SciFileShare\ ( mkdir H:\SciFileShare )
 if not exist P:\SciFileShare\ ( mkdir P:\SciFileShare )
 
-echo aby pobraæ plik od kogoœ stwórz w folderze SciFileShare plik w ten sposób [u¿ytkownik udostêpniaj¹cy plik]RQ.txt > "P:\SciFileShare\1 SciSciFileShare jak uzywac.txt"
-echo np.andrewRQ.txt . w œrodku teko pliku piszesz nazwê pliku który chcesz pobraæ, ten plik powinien siê pojawiæ na publicu  >> "P:\SciFileShare\1 SciSciFileShare jak uzywac.txt"
-echo aby wystawiaæ pliki do pobrania i po wiencej instrukcji zobacz na githuba >> "P:\SciFileShare\1 SciSciFileShare jak uzywac.txt"
-echo https://github.com/sciAndrew/SciSciFileShare >> "P:\SciFileShare\1 SciSciFileShare jak uzywac.txt"
+echo aby pobraæ plik od kogoœ stwórz w folderze SciFileShare plik w ten sposób [u¿ytkownik udostêpniaj¹cy plik]RQ.txt > "P:\SciFileShare\1 SciFileShare jak uzywac.txt"
+echo np.andrewRQ.txt . w œrodku teko pliku piszesz nazwê pliku który chcesz pobraæ, ten plik powinien siê pojawiæ na publicu  >> "P:\SciFileShare\1 SciFileShare jak uzywac.txt"
+echo aby wystawiaæ pliki do pobrania i po wiencej instrukcji zobacz na githuba >> "P:\SciFileShare\1 SciFileShare jak uzywac.txt"
+echo https://github.com/sciAndrew/SciFileShare >> "P:\SciFileShare\1 SciFileShare jak uzywac.txt"
 
 :start
 if %TIME:~3,2%==00 ( del /f /q /s P:\SciFileShare\ && timeout 60 && goto restart )
